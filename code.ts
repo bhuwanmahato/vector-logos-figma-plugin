d0467b4743767d2a8d161a92d0feeb69b4137f20
bhuwanmahato <bhuwan8501@gmail.com>
2025-01-17 23:18:16 +0530

    plugin update commit

--- code.ts
+++ code.ts
@@ -5,13 +5,19 @@ figma.ui.onmessage = async ({ logoText, name }) => {
   // create the svg from text and attempt to position in screen
   const svg = figma.createNodeFromSvg(logoText);
   svg.name = name;
-  svg.x = figma.viewport.center.x;
-  svg.y = figma.viewport.center.y;
+
+  // Resize to 48px height while maintaining aspect ratio
+  const scale = 48 / svg.height;
+  svg.resize(svg.width * scale, 48);
+
+  // Center the logo in the viewport
+  svg.x = figma.viewport.center.x - svg.width / 2;
+  svg.y = figma.viewport.center.y - svg.height / 2;
 
   // select and scroll into view, sometimes this doesn't
   // work so well and i'm not sure why
   figma.currentPage.selection = [svg];
   figma.viewport.scrollAndZoomIntoView([svg]);
 
-  figma.closePlugin();
+  //figma.closePlugin();
 };
